e0af1a46c10553cafd10fefad58250d8d8e1d9d9
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2022-01-16 13:42:42 +0700

    add file serenity runner

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberSerenityRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberSerenityRunner.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features/account/register.feature",
+        tags = ""
+)
+public class TestSuite {
+}
